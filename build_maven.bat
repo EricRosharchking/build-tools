a3a69fd49597c75ff3c157fc65bc197a6e178650
Liyuan Hong <liyuan_hong@epam.com>
2022-04-01 17:40:43 +0800

    build_maven.bat to display info when running Add */target/* to gitignore list and remove corresponding files from repo

--- build_maven.bat
+++ build_maven.bat
@@ -1,1 +1,10 @@
-mvn install 
+echo "Start building & installing artifacts in FIVE seconds"
+timeout /t 5
+call mvn install
+echo Installing artifacts complete, press "any key" to list artifacts structure
+pause
+echo calling "jar tf .\admin\target\admin-1.0-SNAPSHOT.jar"
+call jar tf .\admin\target\admin-1.0-SNAPSHOT.jar
+echo calling "jar tf .\web\target\web-1.0-SNAPSHOT.war"
+call jar tf .\web\target\web-1.0-SNAPSHOT.war
+pause
